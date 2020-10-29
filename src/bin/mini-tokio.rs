420c82f405746c5ff16309ee31b2c9832ddd409a
dslchd@qq.com <dsl816816dsl>
2020-10-29 22:44:34 +0800

    add MiniTokio a little code

--- src/bin/mini-tokio.rs
+++ src/bin/mini-tokio.rs
@@ -1,8 +1,23 @@
 use std::collections::VecDeque;
-use futures::task;
+use futures::{task, Future};
+use std::pin::Pin;
 
 fn main() {}
 
 struct MiniTokio {
     tasks: VecDeque<task>,
+}
+
+type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {tasks: VecDeque::new()}
+    }
+
+    fn spawn<F>(&mut self, future: F)
+    where F:Future<Output = ()> + Send + 'static,
+    {
+        self.tasks.push_back(Box::pin(future));
+    }
 }
